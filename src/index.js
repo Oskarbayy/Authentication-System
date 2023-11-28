cc98ddc6fe394dbe0ae97f965af993e07801c1db
Zub <oskarbayy@gmail.com>
2023-11-28 17:33:40 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
+// get modules
 const express = require("express")
 const app = express()
 const path = require("path")
 const hbs = require("hbs")
+const constructor = require("./mongodb") // get the module with the constructor function
 
-app.use(express.json())
-app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "../templates"))
+app.use(express.json()) // setup middleware
+app.set("view engine", "hbs") // use handlerbar templates
+app.set("views", path.join(__dirname, "../templates")) // change the default 'views' folder to the 'templates' folder
+app.use(express.urlencoded({extended:false}))
 
+// Connections
 app.get("/",(req,res)=>{
     res.render("login")
 })
 
 app.get("/signup",(req,res)=>{
     res.render("signup")
+})
+
+app.post("/signup", async (req,res) => { // connection to '/signup' action in signup.hbs (with async so its a promise)
+    // Store in database
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    }
+
+    console.log(data)
+    console.log(constructor)
+
+    await constructor.insertMany([data])
+
+    // await for it to be inserted into the database and then render home page
+
+    res.render("home")
+
 })
 
 app.listen(3000, ()=> {
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,3 +1,27 @@
 const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost:27017/AuthProject")
+.then(()=>{ // if succeeded
+    console.log("database connected")
+})
+
+.catch(()=>{ // if failed
+    console.log("database failed connection")
+})
+
+// Define schema
+const LoginSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+// create constructor function
+const LoginModel = new mongoose.model("Login", LoginSchema)
+
+module.exports=LoginModel
